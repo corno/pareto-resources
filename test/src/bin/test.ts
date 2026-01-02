dbc09c5b3dc600ddc2e28125e5ceabd375b5bf44
Corno Schraverus <corno@schraverus.com>
2026-01-02 17:45:49 +0100

    ...

--- test/src/bin/test.ts
+++ test/src/bin/test.ts
@@ -2,40 +2,39 @@
 
 import * as _pt from 'pareto-core-transformer'
 import * as _pdev from 'pareto-core-dev'
-import * as _pinternals from 'pareto-core-internals'
 
-import * as r_non_normalized_path from "pub/dist/implementation/manual/schemas/non_normalized_path/deserializers"
+// import * as r_non_normalized_path from "pub/dist/implementation/manual/schemas/non_normalized_path/deserializers"
 
-import * as r_node_path from "pub/dist/implementation/manual/schemas/node_path/refiners/non_normalized_path"
+// import * as r_node_path from "pub/dist/implementation/manual/schemas/node_path/refiners/non_normalized_path"
 
-_pdev.log_debug_message("BEGIN", () => {})
+// _pdev.log_debug_message("BEGIN", () => {})
 
-const x = r_non_normalized_path.Non_Normalized_Path("/a//b/../c/./d")
-_pdev.log_debug_message(`leading slash: ${x['leading slash']}`, () => { })
-x.segments.__for_each(($) => {
-    _pdev.log_debug_message(`segment: ${$[0]}`, () => { })
-})
-_pdev.log_debug_message(`trailing slash: ${x['trailing slash']}`, () => { })
-
-
-//
-
-const $ = r_node_path.Node_Path(
-    x,
-    ($) => _pinternals.panic(`aborting due to error: ${$[0]}`),
-    {
-        'pedantic': true,
-    },
-)
-
-_pt.cc($.context.start, ($) => {
-    switch ($[0]) {
-        case 'absolute': return _pt.ss($, ($) => _pdev.log_debug_message(`abs`, () => {}))
-        case 'relative': return _pt.ss($, ($) => _pdev.log_debug_message(`rel up steps: ${$['up steps']}`, () => {}))
-        default: return _pt.au($[0])
-    }
-})
+// const x = r_non_normalized_path.Non_Normalized_Path("/a//b/../c/./d")
+// _pdev.log_debug_message(`leading slash: ${x['leading slash']}`, () => { })
+// x.segments.__for_each(($) => {
+//     _pdev.log_debug_message(`segment: ${$[0]}`, () => { })
+// })
+// _pdev.log_debug_message(`trailing slash: ${x['trailing slash']}`, () => { })
 
 
-_pdev.log_debug_message("END", () => {})
+// //
+
+// const $ = r_node_path.Node_Path(
+//     x,
+//     ($) => _pinternals.panic(`aborting due to error: ${$[0]}`),
+//     {
+//         'pedantic': true,
+//     },
+// )
+
+// _pt.cc($.context.start, ($) => {
+//     switch ($[0]) {
+//         case 'absolute': return _pt.ss($, ($) => _pdev.log_debug_message(`abs`, () => {}))
+//         case 'relative': return _pt.ss($, ($) => _pdev.log_debug_message(`rel up steps: ${$['up steps']}`, () => {}))
+//         default: return _pt.au($[0])
+//     }
+// })
+
+
+// _pdev.log_debug_message("END", () => {})
 
