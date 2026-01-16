a037135593dbb46e4c5e80d80c3678a6780a5260
Corno Schraverus <corno@schraverus.com>
2026-01-16 16:09:59 +0100

    ...

--- pub/src/implementation/manual/schemas/execute_query_executable/transformers/fountain_pen.ts
+++ pub/src/implementation/manual/schemas/execute_query_executable/transformers/fountain_pen.ts
@@ -17,7 +17,9 @@ export const Error: signatures.Error = ($) => _p.sg($, ($) => {
     switch ($[0]) {
         case 'failed to spawn': return _p.ss($, ($) => sh.b.sub([
             sh.b.snippet(`failed to spawn process:`),
-            sh.b.list($.message.__l_map(($) => sh.b.snippet($)))
+            sh.b.indent([
+                sh.g.sub($['message as list'].__l_map(($) => sh.g.simple_block($)))
+            ])
         ]))
         case 'non zero exit code': return _p.ss($, ($) => sh.b.sub([
             sh.b.snippet(`non zero exit code:`),
@@ -32,7 +34,7 @@ export const Error: signatures.Error = ($) => _p.sg($, ($) => {
                 sh.g.nested_block([
                     sh.b.snippet(`output:`),
                     sh.b.indent([
-                        sh.g.sub($.stderr.__l_map(($) => sh.g.simple_block($)))
+                        sh.g.sub($['stderr as list'].__l_map(($) => sh.g.simple_block($)))
                     ])
                 ])
             ])
--- pub/src/interface/generated/pareto/schemas/execute_query_executable/data.ts
+++ pub/src/interface/generated/pareto/schemas/execute_query_executable/data.ts
@@ -4,15 +4,15 @@ import * as _pi from "pareto-core-interface"
 import * as i__location from "../../core/location"
 
 export namespace Parameters_ {
-    
+
     export namespace args {
-        
+
         export type L = string
-        
+
     }
-    
+
     export type args = _pi.List<args.L>
-    
+
 }
 
 export type Parameters_ = {
@@ -20,54 +20,61 @@ export type Parameters_ = {
 }
 
 export namespace Error_ {
-    
+
     export namespace failed_to_spawn {
-        
-        export type message = _pi.List<string>
-        
+
+        export type message = string
+        export type message_as_list = _pi.List<string>
+
     }
-    
+
     export type failed_to_spawn = {
         readonly 'message': failed_to_spawn.message
+        readonly 'message as list': failed_to_spawn.message_as_list
     }
-    
+
     export namespace non_zero_exit_code {
-        
+
         export namespace exit_code {
-            
+
             export type O = number
-            
+
         }
-        
+
         export type exit_code = _pi.Optional_Value<exit_code.O>
-        
-        export type stderr = _pi.List<string>
-        
+
+        export type stderr = string
+        export type stderr_as_list = _pi.List<string>
+
     }
-    
+
     export type non_zero_exit_code = {
         readonly 'exit code': non_zero_exit_code.exit_code
         readonly 'stderr': non_zero_exit_code.stderr
+        readonly 'stderr as list': non_zero_exit_code.stderr_as_list
     }
-    
+
 }
 
-export type Error_ = 
+export type Error_ =
     | readonly ['failed to spawn', Error_.failed_to_spawn]
     | readonly ['non zero exit code', Error_.non_zero_exit_code]
 
 export namespace Result_ {
-    
-    export type stdout = _pi.List<string>
-    
+
+    export type stdout = string
+    export type stdout_as_list = _pi.List<string>
+
 }
 
 export type Result_ = {
     readonly 'stdout': Result_.stdout
+    readonly 'stdout as list': Result_.stdout_as_list
+    
 }
 
-export { 
-    Parameters_ as Parameters, 
-    Error_ as Error, 
-    Result_ as Result, 
+export {
+    Parameters_ as Parameters,
+    Error_ as Error,
+    Result_ as Result,
 }
