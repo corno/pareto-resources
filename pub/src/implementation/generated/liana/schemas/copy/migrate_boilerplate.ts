e1711687d2c553c4ae32365dbd23d6d6e1eca999
Corno Schraverus <corno@schraverus.com>
2026-01-25 21:14:36 +0100

    ...

--- pub/src/implementation/generated/liana/schemas/copy/migrate_boilerplate.ts
+++ pub/src/implementation/generated/liana/schemas/copy/migrate_boilerplate.ts
@@ -6,26 +6,28 @@ import * as t_signatures from "../../../../../interface/generated/liana/schemas/
 import * as t_out from "../../../../../interface/generated/liana/schemas/copy/data"
 
 import * as v_path from "../path/migrate_boilerplate"
-export const Parameters: t_signatures.Parameters = ($) => ({
-    'source': _p.deprecated_cc($['source'], ($) => v_path.Node_Path(
-        $
-    )),
-    'target': _p.deprecated_cc($['target'], ($) => v_path.Node_Path(
-        $
-    )),
-    'options': _p.deprecated_cc($['options'], ($) => ({
-        'recursive': _p.deprecated_cc($['recursive'], ($) => $.__o_map(($) => $)),
-        'force': _p.deprecated_cc($['force'], ($) => $.__o_map(($) => $)),
-        'errorOnExist': _p.deprecated_cc($['errorOnExist'], ($) => $.__o_map(($) => $)),
+export const Parameters: t_signatures.Parameters = ($,) => ({
+    'source': _p.deprecated_cc($['source'], ($,) => v_path.Node_Path($)),
+    'target': _p.deprecated_cc($['target'], ($,) => v_path.Node_Path($)),
+    'options': _p.deprecated_cc($['options'], ($,) => ({
+        'recursive': _p.deprecated_cc($['recursive'], ($,) => $.__o_map(($,) => $)),
+        'force': _p.deprecated_cc($['force'], ($,) => $.__o_map(($,) => $)),
+        'errorOnExist': _p.deprecated_cc($['errorOnExist'], ($,) => $.__o_map(($,) => $)),
     })),
 })
-export const Error: t_signatures.Error = ($) => _p.deprecated_cc($, ($): t_out.Error => {
+export const Error: t_signatures.Error = ($,) => _p.decide.state($, ($,): t_out.Error => {
     switch ($[0]) {
-        case 'source does not exist': return _p.ss($, ($) => ['source does not exist', null])
-        case 'node is not a file': return _p.ss($, ($) => ['node is not a file', null])
-        case 'permission denied': return _p.ss($, ($) => ['permission denied', null])
-        case 'file too large': return _p.ss($, ($) => ['file too large', null])
-        case 'device not ready': return _p.ss($, ($) => ['device not ready', null])
-        default: return _p.au($[0])
+        case 'source does not exist':
+            return _p.ss($, ($,) => ['source does not exist', null])
+        case 'node is not a file':
+            return _p.ss($, ($,) => ['node is not a file', null])
+        case 'permission denied':
+            return _p.ss($, ($,) => ['permission denied', null])
+        case 'file too large':
+            return _p.ss($, ($,) => ['file too large', null])
+        case 'device not ready':
+            return _p.ss($, ($,) => ['device not ready', null])
+        default:
+            return _p.au($[0])
     }
 })
--- pub/src/implementation/generated/liana/schemas/read_file/migrate_boilerplate.ts
+++ pub/src/implementation/generated/liana/schemas/read_file/migrate_boilerplate.ts
@@ -6,17 +6,21 @@ import * as t_signatures from "../../../../../interface/generated/liana/schemas/
 import * as t_out from "../../../../../interface/generated/liana/schemas/read_file/data"
 
 import * as v_path from "../path/migrate_boilerplate"
-export const Parameters: t_signatures.Parameters = ($) => v_path.Node_Path(
-    $
-)
-export const Error: t_signatures.Error = ($) => _p.deprecated_cc($, ($): t_out.Error => {
+export const Parameters: t_signatures.Parameters = ($,) => v_path.Node_Path($)
+export const Error: t_signatures.Error = ($,) => _p.decide.state($, ($,): t_out.Error => {
     switch ($[0]) {
-        case 'file does not exist': return _p.ss($, ($) => ['file does not exist', null])
-        case 'node is not a file': return _p.ss($, ($) => ['node is not a file', null])
-        case 'permission denied': return _p.ss($, ($) => ['permission denied', null])
-        case 'file too large': return _p.ss($, ($) => ['file too large', null])
-        case 'device not ready': return _p.ss($, ($) => ['device not ready', null])
-        default: return _p.au($[0])
+        case 'file does not exist':
+            return _p.ss($, ($,) => ['file does not exist', null])
+        case 'node is not a file':
+            return _p.ss($, ($,) => ['node is not a file', null])
+        case 'permission denied':
+            return _p.ss($, ($,) => ['permission denied', null])
+        case 'file too large':
+            return _p.ss($, ($,) => ['file too large', null])
+        case 'device not ready':
+            return _p.ss($, ($,) => ['device not ready', null])
+        default:
+            return _p.au($[0])
     }
 })
-export const Result: t_signatures.Result = ($) => $
+export const Result: t_signatures.Result = ($,) => $
