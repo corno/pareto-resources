6da734998fce8e170258d8b7b588af0471c9d8f1
Corno Schraverus <corno@schraverus.com>
2026-02-03 13:50:50 +0100

    .

--- pub/src/implementation/manual/schemas/path/transformers/text.ts
+++ pub/src/implementation/manual/schemas/path/transformers/text.ts
@@ -1,27 +1,35 @@
 import * as _p from 'pareto-core/dist/expression'
-import _p_text_build_deprecated from 'pareto-core/dist/_p_text_build_deprecated'
+import _p_list_build_deprecated from 'pareto-core/dist/_p_list_build_deprecated'
+import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
 
 import * as d_in from "../../../../../interface/generated/liana/schemas/path/data"
+import * as d_out from "pareto-fountain-pen/dist/interface/to_be_generated/text"
 
-export const Node_Path = ($: d_in.Node_Path): string => {
-    return `${Context_Path($.context)}/${$.node}`
+export const Node_Path = ($: d_in.Node_Path): d_out.Text => {
+    return _p.list.nested_literal_old([
+        Context_Path($.context),
+        [
+            47, // '/'
+        ],
+        _p_list_from_text($.node, ($) => $)
+    ])
 }
 
-export const Context_Path = ($: d_in.Context_Path): string => {
-    return _p_text_build_deprecated(($i) => {
+export const Context_Path = ($: d_in.Context_Path): d_out.Text => {
+    return _p_list_build_deprecated(($i) => {
         _p.decide.state($.start, ($) => {
             switch ($[0]) {
                 case 'absolute': return _p.ss($, ($) => {
                     // $i.add_character(47) // '/'
                 })
                 case 'relative': return _p.ss($, ($) => {
-                    $i.add_character(46) // .
+                    $i['add item'](46) // .
 
                     let k = $['up steps']
                     while (k > 0) {
-                        $i.add_character(47) // /
-                        $i.add_character(46) // .
-                        $i.add_character(46) // .
+                        $i['add item'](47) // /
+                        $i['add item'](46) // .
+                        $i['add item'](46) // .
 
                         k -= 1
                     }
@@ -30,11 +38,11 @@ export const Context_Path = ($: d_in.Context_Path): string => {
             }
         })
         if (_p.boolean.list_is_empty($.subpath) && $.start[0] === 'absolute') {
-            $i.add_character(47) // '/'
+            $i['add item'](47) // '/'
         }
         $.subpath.__l_map(($) => {
-            $i.add_character(47) // '/'
-            $i.add_snippet($)
+            $i['add item'](47) // '/'
+            $i['add list'](_p_list_from_text($, ($) => $))
         })
     })
 }
