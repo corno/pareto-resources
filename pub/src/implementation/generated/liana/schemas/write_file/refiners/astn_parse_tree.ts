2b943dc116c14fe9534be5a2b2cc28ed600d3bb5
Corno Schraverus <corno@schraverus.com>
2026-02-05 20:38:51 +0100

    pdt: ...

---
+++ pub/src/implementation/generated/liana/schemas/write_file/refiners/astn_parse_tree.ts
@@ -0,0 +1,91 @@
+    
+    import * as _p from "pareto-core/dist/expression"
+    
+    import _p_change_context from "pareto-core/dist/_p_change_context"
+    
+    import _p_list_from_text from "pareto-core/dist/_p_list_from_text"
+    
+    import * as t_signatures from "../../../../../../interface/generated/liana/schemas/write_file/unmarshall"
+    
+    import * as t_out from "../../../../../../interface/generated/liana/schemas/write_file/data"
+    
+    import * as v_deserialize_number from "liana-core/dist/implementation/manual/primitives/integer/deserializers/decimal"
+    
+    import * as v_deserialize_boolean from "liana-core/dist/implementation/manual/primitives/boolean/deserializers/true_false"
+    
+    import * as v_unmarshalled_from_parse_tree from "astn-core/dist/implementation/manual/schemas/unmarshalled/refiners/parse_tree"
+    
+    import * as v_parse_tree_to_location from "astn-core/dist/implementation/manual/schemas/parse_tree/transformers/location"
+    
+    import * as v_external_path from "../../path/refiners/astn_parse_tree"
+    
+    import * as v_external_list_of_characters from "../../list_of_characters/refiners/astn_parse_tree"
+    
+    export const Parameters: t_signatures.Parameters = ($, abort) => _p_change_context(
+        v_unmarshalled_from_parse_tree.Group(
+            $,
+            ($) => abort(
+                ['expected a group', null],
+            ),
+        ),
+        ($) => ({
+            'path': _p_change_context(
+                $.__get_entry(
+                    'path',
+                    ($) => abort(
+                        ['no such entry', "path"],
+                    ),
+                ),
+                ($) => v_external_path.Node_Path(
+                    $,
+                    ($) => abort(
+                        $,
+                    ),
+                ),
+            ),
+            'data': _p_change_context(
+                $.__get_entry(
+                    'data',
+                    ($) => abort(
+                        ['no such entry', "data"],
+                    ),
+                ),
+                ($) => v_external_list_of_characters.List_of_Characters(
+                    $,
+                    ($) => abort(
+                        $,
+                    ),
+                ),
+            ),
+        }),
+    )
+    
+    export const Error: t_signatures.Error = ($, abort) => _p_change_context(
+        v_unmarshalled_from_parse_tree.State(
+            $,
+            ($) => abort(
+                ['expected a state', null],
+            ),
+        ),
+        ($) => _p.decide.text(
+            $['option']['value'],
+            ($t): t_out.Error => {
+                switch ($t) {
+                    case 'permission denied':
+                        return _p_change_context(
+                            $['value'],
+                            ($) => ['permission denied', v_unmarshalled_from_parse_tree.Nothing(
+                                $,
+                                ($) => abort(
+                                    ['expected a nothing', null],
+                                ),
+                            )],
+                        )
+                    default:
+                        return abort(
+                            ['unknown option', $['option']['value']],
+                        )
+                }
+            },
+        ),
+    )
