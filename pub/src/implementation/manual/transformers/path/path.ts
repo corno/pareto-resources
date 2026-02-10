7c56b540c167360afaf05e1fd307bd1633df7419
Corno Schraverus <corno@schraverus.com>
2026-02-10 11:41:46 +0100

    pdt: ...

--- pub/src/implementation/manual/transformers/path/path.ts
+++ pub/src/implementation/manual/transformers/path/path.ts
@@ -1,14 +1,15 @@
 import * as _pi from 'pareto-core/dist/interface'
 import * as _p from 'pareto-core/dist/assign'
 
-import * as d_path from "../../../../interface/generated/liana/schemas/path/data"
+import * as d_out from "../../../../interface/generated/liana/schemas/path/data"
+import * as d_in from "../../../../interface/generated/liana/schemas/path/data"
 
 export const extend_context_path = (
-    $: d_path.Context_Path,
+    $: d_in.Context_Path,
     $p: {
         'addition': string
     }
-): d_path.Context_Path => ({
+): d_out.Context_Path => ({
     'start': $.start,
     'subpath': _p.list.nested_literal_old([
         $.subpath,
@@ -19,11 +20,11 @@ export const extend_context_path = (
 })
 
 export const extend_context_path_with_list = (
-    $: d_path.Context_Path,
+    $: d_in.Context_Path,
     $p: {
         'addition': _pi.List<string>
     }
-): d_path.Context_Path => ({
+): d_out.Context_Path => ({
     'start': $.start,
     'subpath': _p.list.nested_literal_old([
         $.subpath,
@@ -33,28 +34,28 @@ export const extend_context_path_with_list = (
 
 
 export const extend_node_path = (
-    $: d_path.Node_Path,
+    $: d_in.Node_Path,
     $p: {
         'addition': string
     }
-): d_path.Node_Path => ({
+): d_out.Node_Path => ({
     'context': deprecated_node_path_to_context_path($),
     'node': $p.addition,
 })
 
 export const create_node_path = (
-    $: d_path.Context_Path,
+    $: d_in.Context_Path,
     $p: {
         node: string
     }
-): d_path.Node_Path => ({
+): d_out.Node_Path => ({
     'context': $,
     'node': $p.node,
 })
 
 export const deprecated_node_path_to_context_path = (
-    $: d_path.Node_Path
-): d_path.Context_Path => extend_context_path(
+    $: d_in.Node_Path
+): d_out.Context_Path => extend_context_path(
     $.context,
     {
         'addition': $.node
