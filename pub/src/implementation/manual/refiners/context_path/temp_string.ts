eb579a9e33bbadf6cea61cc6a23268f8e6371283
Corno Schraverus <corno@schraverus.com>
2026-02-28 15:32:06 +0100

    pdt: published version 0.2.55

--- pub/src/implementation/manual/refiners/context_path/temp_string.ts
+++ pub/src/implementation/manual/refiners/context_path/temp_string.ts
@@ -3,7 +3,7 @@ import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
 import * as d_out from "../../../../interface/generated/liana/schemas/path/data"
 
 import * as x1 from "./non_normalized_path"
-import * as x2 from "../non_normalized_path/text"
+import * as x2 from "../non_normalized_path/list_of_characters"
 
 export const Context_Path = ( //This one is exceptional because it will never produce errors
     $: string,
--- pub/src/implementation/manual/refiners/node_path/text.ts
+++ pub/src/implementation/manual/refiners/node_path/text.ts
@@ -1,25 +1,29 @@
 import * as _pi from 'pareto-core/dist/interface'
+import * as _p from 'pareto-core/dist/assign'
+import _p_list_from_text from 'pareto-core/dist/_p_list_from_text'
 
+//data types
 import * as d_out from "../../../../interface/generated/liana/schemas/path/data"
-import * as d_in from "pareto-fountain-pen/dist/interface/generated/liana/schemas/list_of_characters/data"
+import * as d_in from "pareto-fountain-pen/dist/interface/generated/liana/schemas/text/data"
 import * as d_error from "./non_normalized_path"
 
 export type Parameters = { 'pedantic': boolean }
 
 export namespace signatures {
-    export type Node_Path = _pi.Refiner_With_Parameter<d_out.Node_Path, d_error.Error, d_in.List_of_Characters, Parameters>
+    export type Node_Path = _pi.Refiner_With_Parameter<d_out.Node_Path, d_error.Error, d_in.Text, Parameters>
 }
 
-import * as _pt from 'pareto-core/dist/assign'
 
 //dependencies
-import * as x1 from "./non_normalized_path"
-import * as x2 from "../non_normalized_path/text"
+import * as t_from_list_of_characters from "./list_of_characters"
 
 
 export const Node_Path: signatures.Node_Path = ($, abort, $p) => {
-    return x1.Node_Path(
-        x2.Non_Normalized_Path($),
+    return t_from_list_of_characters.Node_Path(
+        _p_list_from_text(
+            $,
+            ($) => $
+        ),
         abort,
         $p,
     )
