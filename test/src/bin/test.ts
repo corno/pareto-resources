88d02457261a2bf53c54c026fd2bfc4ec9eb9ff0
Corno Schraverus <corno@schraverus.com>
2026-03-13 09:36:03 +0100

    pdt: ...

--- test/src/bin/test.ts
+++ test/src/bin/test.ts
@@ -6,14 +6,14 @@
 
 // import * as r_node_path from "pub/dist/implementation/manual/schemas/node_path/refiners/non_normalized_path"
 
-// _pdev.log_debug_message("BEGIN", () => {})
+// _p_log_debug_message("BEGIN", () => {})
 
 // const x = r_non_normalized_path.Non_Normalized_Path("/a//b/../c/./d")
-// _pdev.log_debug_message(`leading slash: ${x['leading slash']}`, () => { })
+// _p_log_debug_message(`leading slash: ${x['leading slash']}`, () => { })
 // x.segments.__for_each(($) => {
-//     _pdev.log_debug_message(`segment: ${$[0]}`, () => { })
+//     _p_log_debug_message(`segment: ${$[0]}`, () => { })
 // })
-// _pdev.log_debug_message(`trailing slash: ${x['trailing slash']}`, () => { })
+// _p_log_debug_message(`trailing slash: ${x['trailing slash']}`, () => { })
 
 
 // //
@@ -28,12 +28,12 @@
 
 // _pt.decide.state($.context.start, ($) => {
 //     switch ($[0]) {
-//         case 'absolute': return _pt.ss($, ($) => _pdev.log_debug_message("abs", () => {}))
-//         case 'relative': return _pt.ss($, ($) => _pdev.log_debug_message(`rel up steps: ${$['up steps']}`, () => {}))
+//         case 'absolute': return _pt.ss($, ($) => _p_log_debug_message("abs", () => {}))
+//         case 'relative': return _pt.ss($, ($) => _p_log_debug_message(`rel up steps: ${$['up steps']}`, () => {}))
 //         default: return _pt.au($[0])
 //     }
 // })
 
 
-// _pdev.log_debug_message("END", () => {})
+// _p_log_debug_message("END", () => {})
 
