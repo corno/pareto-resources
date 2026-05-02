a2fa25e35d6029e56d4240709439e4787cc012a3
Corno Schraverus <corno@schraverus.com>
2026-05-02 11:28:36 +0200

    pdt: ...

---
+++ typescript/lib/src/implementation/manual/transformers/stat_possible_node/fountain_pen.ts
@@ -0,0 +1,25 @@
+import * as _p from 'pareto-core/dist/assign'
+import * as _pi from 'pareto-core/dist/interface'
+
+//data types
+import * as d_in from "../../../../interface/generated/liana/schemas/stat_possible_node/data"
+import * as d_out from "pareto-fountain-pen/dist/interface/generated/liana/schemas/prose/data"
+
+export type Error = _pi.Transformer<d_in.Error, d_out.Phrase>
+
+//dependencies
+import * as t_path_to_text from "../path/list_of_characters"
+
+//shorthands
+import * as sh from "pareto-fountain-pen/dist/shorthands/prose"
+
+export const Error: Error = ($) => sh.ph.composed([
+    _p.decide.state($.type, ($) => {
+        switch ($[0]) {
+            case 'unknown': return _p.ss($, ($) => sh.ph.literal("unknown"))
+            default: return _p.au($[0])
+        }
+    }),
+    sh.ph.literal(": "),
+    sh.ph.serialize(t_path_to_text.Node_Path($.path)),
+])
