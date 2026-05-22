7f4d21ed1e1e5cfff54bf70c6e90be7f41bd48d1
Corno Schraverus <corno@schraverus.com>
2026-05-22 16:39:17 +0200

    pdt: ...

--- typescript/lib/src/implementation/temp/temp_core.ts
+++ typescript/lib/src/implementation/temp/temp_core.ts
@@ -5,7 +5,7 @@ import _p_list_build_deprecated from 'pareto-core/dist/_p_list_build_deprecated'
 //data types
 
 export const remove_last_element = <T>(list: _pi.List<T>): _pi.List<T> => {
-    const length = _p.number.natural.from.list(list).amount_of_items()
+    const length = _p.number.from.list(list).amount_of_items()
     let index = -1
     return _p_list_build_deprecated(($i) => {
         list.__l_map(($) => {
