48444b4beb7dff576ee776e560a46fb14d62f312
Corno Schraverus <corno@schraverus.com>
2026-06-28 12:30:42 +0200

    pdt: ...

--- typescript/lib/src/implementation/manual/queries/resources_read_directory_content.ts
+++ typescript/lib/src/implementation/manual/queries/resources_read_directory_content.ts
@@ -3,7 +3,6 @@ import p_super_query_result from 'pareto-core/dist/implementation/query/super_qu
 
 import p_text_from_list from 'pareto-core/dist/implementation/transformer/specials/text_from_list'
 
-import * as d_directory_content from "../../../interface/data/directory_content"
 import * as d_read_directory_content from "../../../interface/data/read_directory_content"
 
 import * as interface_ from "../../../interface/signatures/resources"
