104ff3a00bb43170285fbbeff264169f6ca71a6c
Corno Schraverus <corno@schraverus.com>
2026-07-07 16:52:57 +0200

    pdt: published version 0.2.135

--- typescript/lib/src/interface/data/directory_structure.ts
+++
@@ -1,8 +0,0 @@
-import * as p_ from 'pareto-core/interface/data'
-
-export type Node =
-    | ['other', null]
-    | ['file', null]
-    | ['directory', Directory]
-
-export type Directory = p_.Dictionary<Node>
--- typescript/lib/src/interface/resources.ts
+++ typescript/lib/src/interface/resources.ts
@@ -7,74 +7,6 @@ import * as d_execute_unrestricted_smelly_command_executable from "./generated/l
 import * as d_execute_sandboxed_command_command from "./generated/liana/schemas/execute_sandboxed_command_executable/data.js"
 import * as d_execute_sandboxed_query_executable from "./generated/liana/schemas/execute_sandboxed_query_executable/data.js"
 import * as d_execute_sandboxed_smelly_command_command from "./generated/liana/schemas/execute_sandboxed_smelly_command_executable/data.js"
-// import * as d_fs_sandboxed_chmod from "./generated/liana/schemas/fs_sandboxed_chmod/data.js"
-// import * as d_fs_sandboxed_copy from "./generated/liana/schemas/fs_sandboxed_copy/data.js"
-// import * as d_fs_sandboxed_make_directory from "./generated/liana/schemas/fs_sandboxed_make_directory/data.js"
-// import * as d_fs_sandboxed_read_directory from "./generated/liana/schemas/fs_sandboxed_read_directory/data.js"
-// import * as d_fs_sandboxed_read_file from "./generated/liana/schemas/fs_sandboxed_read_file/data.js"
-// import * as d_fs_sandboxed_read_file_within_context from "./generated/liana/schemas/fs_sandboxed_read_file_within_context/data.js"
-// import * as d_fs_sandboxed_remove from "./generated/liana/schemas/fs_sandboxed_remove/data.js"
-// import * as d_fs_sandboxed_stat from "./generated/liana/schemas/fs_sandboxed_stat/data.js"
-// import * as d_fs_sandboxed_stat_possible_node from "./generated/liana/schemas/fs_sandboxed_stat_possible_node/data.js"
-// import * as d_fs_sandboxed_write_file from "./generated/liana/schemas/fs_sandboxed_write_file/data.js"
-import * as d_fs_unrestricted_chmod from "./generated/liana/schemas/fs_unrestricted_chmod/data.js"
-import * as d_fs_unrestricted_copy from "./generated/liana/schemas/fs_unrestricted_copy/data.js"
-import * as d_fs_unrestricted_make_directory from "./generated/liana/schemas/fs_unrestricted_make_directory/data.js"
-import * as d_fs_unrestricted_read_directory from "./generated/liana/schemas/fs_unrestricted_read_directory/data.js"
-import * as d_fs_unrestricted_read_file from "./generated/liana/schemas/fs_unrestricted_read_file/data.js"
-import * as d_fs_unrestricted_remove from "./generated/liana/schemas/fs_unrestricted_remove/data.js"
-import * as d_fs_unrestricted_stat from "./generated/liana/schemas/fs_unrestricted_stat/data.js"
-import * as d_fs_unrestricted_stat_possible_node from "./generated/liana/schemas/fs_unrestricted_stat_possible_node/data.js"
-import * as d_fs_unrestricted_write_file from "./generated/liana/schemas/fs_unrestricted_write_file/data.js"
-import * as d_read_directory_content from "./data/read_directory_content.js"
-import * as d_read_directory_structure from "./data/read_directory_structure.js"
-import * as d_write_directory_content from "./data/write_directory_content.js"
-
-
-export namespace filesystem_unrestricted {
-
-    export namespace commands {
-
-        export type chmod = p_ci.Command<d_fs_unrestricted_chmod.Error, d_fs_unrestricted_chmod.Parameters>
-        export type copy = p_ci.Command<d_fs_unrestricted_copy.Error, d_fs_unrestricted_copy.Parameters>
-        export type make_directory = p_ci.Command<d_fs_unrestricted_make_directory.Error, d_fs_unrestricted_make_directory.Parameters>
-        export type remove = p_ci.Command<d_fs_unrestricted_remove.Error, d_fs_unrestricted_remove.Parameters>
-        export type write_file = p_ci.Command<d_fs_unrestricted_write_file.Error, d_fs_unrestricted_write_file.Parameters>
-
-    }
-
-    export namespace queries {
-
-        export type read_directory = p_qi.Query<d_fs_unrestricted_read_directory.Result, d_fs_unrestricted_read_directory.Error, d_fs_unrestricted_read_directory.Parameters>
-        export type read_file = p_qi.Query<d_fs_unrestricted_read_file.Result, d_fs_unrestricted_read_file.Error, d_fs_unrestricted_read_file.Parameters>
-        export type stat = p_qi.Query<d_fs_unrestricted_stat.Result, d_fs_unrestricted_stat.Error, d_fs_unrestricted_stat.Parameters>
-        export type stat_possible_node = p_qi.Query<d_fs_unrestricted_stat_possible_node.Result, d_fs_unrestricted_stat_possible_node.Error, d_fs_unrestricted_stat_possible_node.Parameters>
-
-    }
-}
-
-// export namespace filesystem_sandboxed {
-
-//     export namespace commands {
-
-//         export type chmod = p_ci.Command<d_fs_sandboxed_chmod.Error, d_fs_sandboxed_chmod.Parameters>
-//         export type copy = p_ci.Command<d_fs_sandboxed_copy.Error, d_fs_sandboxed_copy.Parameters>
-//         export type make_directory = p_ci.Command<d_fs_sandboxed_make_directory.Error, d_fs_sandboxed_make_directory.Parameters>
-//         export type remove = p_ci.Command<d_fs_sandboxed_remove.Error, d_fs_sandboxed_remove.Parameters>
-//         export type write_file = p_ci.Command<d_fs_sandboxed_write_file.Error, d_fs_sandboxed_write_file.Parameters>
-
-//     }
-
-//     export namespace queries {
-
-//         export type read_directory = p_qi.Query<d_fs_sandboxed_read_directory.Result, d_fs_sandboxed_read_directory.Error, d_fs_sandboxed_read_directory.Parameters>
-//         export type read_file = p_qi.Query<d_fs_sandboxed_read_file.Result, d_fs_sandboxed_read_file.Error, d_fs_sandboxed_read_file.Parameters>
-//         export type read_file_within_context = p_qi.Query<d_fs_sandboxed_read_file_within_context.Result, d_fs_sandboxed_read_file_within_context.Error, d_fs_sandboxed_read_file_within_context.Parameters>
-//         export type stat = p_qi.Query<d_fs_sandboxed_stat.Result, d_fs_sandboxed_stat.Error, d_fs_sandboxed_stat.Parameters>
-//         export type stat_possible_node = p_qi.Query<d_fs_sandboxed_stat_possible_node.Result, d_fs_sandboxed_stat_possible_node.Error, d_fs_sandboxed_stat_possible_node.Parameters>
-
-//     }
-// }
 
 export namespace execute_unrestricted {
 
@@ -106,20 +38,4 @@ export namespace execute_sandboxed {
         export type query_executable = p_qi.Query<d_execute_sandboxed_query_executable.Result, d_execute_sandboxed_query_executable.Error, d_execute_sandboxed_query_executable.Parameters>
 
     }
-}
-
-export namespace resources {
-
-    export namespace commands {
-
-        export type write_directory_content = p_ci.Command<d_write_directory_content.Error, d_write_directory_content.Parameters>
-    }
-
-    export namespace queries {
-
-        export type read_directory_content = p_qi.Query<d_read_directory_content.Result, d_read_directory_content.Error, d_read_directory_content.Parameters>
-        export type read_directory_structure = p_qi.Query<d_read_directory_structure.Result, d_read_directory_structure.Error, d_read_directory_structure.Parameters>
-
-    }
-
 }
