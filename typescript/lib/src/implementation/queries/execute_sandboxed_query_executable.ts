4b013ce62aa35a0db9dd24f91a29f5021217757f
Corno Schraverus <corno@schraverus.com>
2026-07-12 13:24:19 +0200

    pdt: ...

--- typescript/lib/src/implementation/queries/execute_sandboxed_query_executable.ts
+++ typescript/lib/src/implementation/queries/execute_sandboxed_query_executable.ts
@@ -1,11 +1,19 @@
 import * as p_ from 'pareto-core/implementation/query'
 
-import type * as interface_ from "../../declarations/queries.js"
+import type * as query_interfaces from "../../interface/queries.js"
 
 //dependencies
 
 
-export const $$: interface_.query_executable = p_.query(
+export const $$: p_.Query_Implementation<
+    query_interfaces.execute_sandboxed.query_executable,
+    {
+        'program': string,
+    },
+    {
+        'unrestricted': query_interfaces.execute_unrestricted.query_executable,
+    }
+> = p_.query(
     ($d, $s, $q) => $q.unrestricted(
         {
             'program': $s.program,
